c34c15ae7cb72033be27766f10e0b11ec61b805d
loulevin <patricia.balbier@gmail.com>
2023-08-16 15:47:10 +0200

    add documentauon map

--- src/pages/PageDocumentation.ts
+++ src/pages/PageDocumentation.ts
@@ -5,6 +5,13 @@ export const PageDocumentation = () => {
   return /*html*/ `
     <div class="page pageDocumentation">
     <p>There are ${documentationLinks.length}</p>
+    ${documentationLinks.map(link => {
+      return /*html*/ `
+      <div>
+        nnn
+      </div>
+      `
+    }).join("")}
 </div>      
 `;
 };
