e35f2c9c4a59d0d7da77aefaf582a6cc9c85a1c0
loulevin <patricia.balbier@gmail.com>
2023-08-16 15:37:12 +0200

    add documentation links

--- src/pages/PageDocumentation.ts
+++ src/pages/PageDocumentation.ts
@@ -1,7 +1,10 @@
+import links from "../data/links.json";
+const documentationLinks = links.filter(m => m.category === "Documentation");
+
 export const PageDocumentation = () => {
   return /*html*/ `
     <div class="page pageDocumentation">
-    <p>This is the documentation page</p>
+    <p>There are ${documentationLinks.length}</p>
 </div>      
 `;
 };
